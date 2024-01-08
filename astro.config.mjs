2bb649946cc8dc1fc20f18b938324474b1de0106
David Cruwys <david@ideasmen.com.au>
2024-01-08 10:39:51 +1000

    sailent theme and getting ready to convert components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
+import { PORT, SITE } from './src/consts';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: SITE,
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  server: {
+    port: PORT
+  }
+
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE = 'https://klueless.io'
+export const SITE_TITLE = 'Klueless.io'
+export const SITE_DESCRIPTION = 'Coding with GPT'
+export const PORT = 5020
+export const POSTS_PER_PAGE = 4
